6c7b1826da00acc6d422a56ebb2273fc9ea39f17
Rebecca Mark <rebecca.lmark@gmail.com>
2015-03-23 12:33:03 -0700

    Blargh, not working yet

--- src/com/mark/rebecca/Kill.scala
+++ src/com/mark/rebecca/Kill.scala
@@ -5,7 +5,7 @@ class Kill (length: Int, start: Int, skip: Int){
   var circularList = new CircularList( length ).makeList
 
   // the start function should do cycle detection and return the node whose value is = to integer
-  def startPoint(circularList : Node, start : Int, depth : Int = 0) : Node = {
+  def startPoint(circularList : Node, start : Int, depth : Int ) : Node = {
     if (depth > length) {
       throw new IllegalArgumentException("Hey that start point is not in the list.")
     }
@@ -17,18 +17,25 @@ class Kill (length: Int, start: Int, skip: Int){
   }
 
   // still need to reassign last node's next to itself... and remove things from circle?
-  def skipUntilLast(startingNode : Node, skip : Int) : Node = {
-    //advance through list
+  // maybe a way to treat startingNode as a var in signature?
+  def skipUntilLast( startingNode : Node, skip : Int) : Node = {
+   // startingNode needs to be mutatable, so reassign to var
+    var variableNode = startingNode
+
+    // advance through list and return the node to kill off.
     for (i <- 1 to skip) {
-      startingNode.next
-    }
-    // do reassignment here
+      variableNode = variableNode.next
+    } yield variableNode
+
+    // remove the person by resetting the next pointer to skip them.
+    // reassign startingNode to the person after the removal.
+    variableNode.next = variableNode.next.next
 
     // basecase, but I still need to reassign last node to itself...
-    if (startingNode.next == startingNode) {
-      startingNode
+    if (variableNode.next == variableNode) {
+      variableNode
     } else {
-      skipUntilLast(startingNode.next , skip)
+      skipUntilLast(variableNode , skip)
     }
   }
 }
